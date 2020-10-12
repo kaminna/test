b09c55a3c3a9d056e546789927ec8f797656fd08
kaminna <71122975+kaminna@users.noreply.github.com>
2020-10-12 15:12:55 +0800

    Update code2.c
    比较三个数大小并输出最大的数。

--- code2.c
+++ code2.c
@@ -1,7 +1,14 @@
 #include<stdio.h>
 int main(void)
 {
-  int a,b,c,max;
-  
+  int a,b,c,max,a1,b1,c1;
+  a1=a;b1=b;c1=c;
+  scanf("请输入三个数：%d%d%d",&a,&b,&c);
+  max=a<b?b:a;
+  if(max<c)
+  {
+    max=c;
+  }
+  printf("%d,%d,%d这三个数中最大的数为：%d",a1,b1,c1,max);
   return 0;
 }

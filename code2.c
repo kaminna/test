9c93949c03440a6318d0c385daa35d8a57341d10
kaminna <71122975+kaminna@users.noreply.github.com>
2020-10-12 15:34:08 +0800

    Update code2.c

--- code2.c
+++ code2.c
@@ -1,14 +1,24 @@
 #include<stdio.h>
 int main(void)
 {
-  int a,b,c,max,a1,b1,c1;
-  a1=a;b1=b;c1=c;
-  scanf("请输入三个数：%d%d%d",&a,&b,&c);
-  max=a<b?b:a;
-  if(max<c)
-  {
-    max=c;
-  }
-  printf("%d,%d,%d这三个数中最大的数为：%d",a1,b1,c1,max);
-  return 0;
+	int a, b,c,max,a1,b1,c1;
+	scanf("%d%d%d", &a, &b,&c);
+	a1 = a;
+	b1 = b;
+	c1 = c;
+	if (a < b)
+	{
+		max = b;
+	}
+	else
+	{
+		max = a;
+	}
+	if (max < c)
+	{
+		max = c;
+	}
+	printf("%d %d %d中最大的数为：%d\n",a1,b1,c1,max);
+	system("pause");
+	return 0;
 }

d8ba5d4bbf6150d219a03467bc2599e7198382ee
kaminna <71122975+kaminna@users.noreply.github.com>
2020-10-17 18:47:54 +0800

    Create 例2.2.c

---
+++ 例2.2.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+	int i;
+	float f;
+	scanf("%d,%f", &i, &f);      //输入的逗号应为英语的逗号
+	printf("i=%d,f=%f\n",i,f);
+	system("pause");
+	return 0;
+}

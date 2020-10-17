9b37632b671f21bfc72b1fcb840e825e40a07cce
kaminna <71122975+kaminna@users.noreply.github.com>
2020-10-17 18:25:33 +0800

    Create 例1.2.cpp

---
+++ 例1.2.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<stdlib.h>
+using namespace std;
+int main()
+{
+	int a1, a2, result;
+	cout << "please input two number :\n";
+	cin >> a1 >> a2;
+	result = a1*a2;
+	cout << "result is;  " << result << endl;
+	system("pause");
+	return 0;
+}

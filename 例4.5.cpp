f1884f3b7966933c7ec317de6963f4d0eca47ccf
kaminna <71122975+kaminna@users.noreply.github.com>
2020-10-29 10:23:50 +0800

    Create 例4.5.cpp

---
+++ 例4.5.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<cstdlib>
+using namespace std;
+long fact(int m)
+{
+	int i;
+	long s = 1;
+	for (i = 1; i <= m; i++)
+		s = s*i;
+	return(s);
+}
+int main()
+{
+	int n; long p;
+	cin >> n;
+	p = fact(n);
+	cout << p << endl;
+	system("pause");
+	return 0;
+}

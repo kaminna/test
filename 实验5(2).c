0bf00ae7eabe9cb4200fe0f8f7d35fe800997bdd
kaminna <71122975+kaminna@users.noreply.github.com>
2020-11-06 11:08:26 +0800

    Create 实验5(2).c

--- 实验5(2).c
+++ 实验5(2).c
@@ -3,8 +3,8 @@
 int main()
 {
 	const int std = 10;
-	int n,i=0,j=0,sum=0;
-	for (int a = 1; a <= 20; a++)
+	int n,i=0,j=0,sum=0,a;
+	for (a = 1; a <= 20; a++)
 	{
 		printf("请输入第%d根金属杆的长度(cm)：",a);
 		scanf("%d", &n);
@@ -12,15 +12,15 @@ int main()
 		{
 			i++;
 		}
-		else
+		else if(n<10)
 		{
 			j++;
 		}
 		sum += n;
 	}
-	printf("超过标准长度的根数为(根)：%d\n", i);
-	printf("低于标准长度的根数为(根)：%d\n", j);
-	printf("这批金属杆的平均长度为(cm):%d\n", sum / 20);
+	printf("超过标准长度的根数有：%d根\n", i);
+	printf("低于标准长度的根数有：%d根\n", j);
+	printf("这批金属杆的平均长度为:%dcm\n", sum / 20);
 	system("pause");
 	return 0;
 }

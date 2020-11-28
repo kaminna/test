6a31007fe3503a59eab43a28322699ed73d3ab16
kaminna <71122975+kaminna@users.noreply.github.com>
2020-11-28 04:02:10 +0000

    Create 例8.14.cpp

---
+++ 例8.14.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+void swap(int*, int*);
+int main()
+{
+	int a = 3, b = 8;
+	cout << "交换前:" << endl;
+	cout << "a=" << a << "\t	b=" << b << endl;
+	swap(&a, &b);
+	cout << "交换后：" << endl; cout << "a=" << a << "\t	b=" << b << endl;
+	return 0;
+}
+void swap(int *x, int *y)
+{
+	int temp = *x;
+	*x = *y;
+	*y = temp;
+}

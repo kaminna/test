e2e0ac94458ac57027be51392f821a92e263b810
kaminna <71122975+kaminna@users.noreply.github.com>
2020-12-09 12:53:32 +0000

    Create P187)8.c

---
+++ P187)8.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+int main(void)
+{
+	int a[180][180];
+	int m, n;
+	double sum = 0, average[180];
+	printf("请输入二维数组的行列数：\n");
+	scanf("%d%d", &m, &n);
+	int i, j;
+	printf("请输入该二维数组：\n");
+	for (i = 0; i < m; i++)
+	{
+//		printf("请输入第%d行的数字：", i + 1);
+		for (j = 0; j < n; j++)
+			scanf("%d", &a[i][j]);
+	}
+	for (i = 0; i < m; i++)			//计算平均值
+	{
+		for (j = 0; j < n; j++)
+			sum += a[i][j];
+		average[i] = sum / n;
+	}
+	printf("每行的平均值为：");
+	for (i = 0; i < m; i++)			//输出平均值
+	{
+		printf("\n");
+		printf("%f", average[i]);
+	}
+	printf("\n");
+	return 0;
+}

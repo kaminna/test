d25b5ae70b2bd10683cdf42b123b05b18cf4a878
kaminna <71122975+kaminna@users.noreply.github.com>
2020-12-16 16:20:59 +0000

    Update 实验9(3).c

--- 实验9(3).c
+++ 实验9(3).c
@@ -6,37 +6,41 @@ struct student
 	int chinese;
 	int math;
 	int english;
-}stu[5];
+}stu[5], t;
 void print()
 {
-	int i,j,sum[5];
-	struct student t;
-	for(i=0;i<5;i++)
+	int i, j, sum[5],temp;
+	for (i = 0; i<5; i++)
 	{
-		sum[i]=stu[i].chinese+stu[i].math+stu[i].english;
+		sum[i] = stu[i].chinese + stu[i].math + stu[i].english;
 	}
-	for(i=0;i<4;i++)
-		for(j=0;j<4-i;j++)
-			if(sum[j]<sum[j+1])
-			{t=stu[j];stu[j]=stu[j+1];stu[j+1]=t;}
-	for(i=0;i<5;i++)
-		printf("%d	%s	%4d%4d%4d\n",stu[i].num,stu[i].name,stu[i].chinese,stu[i].math,stu[i].english);
+	for (i = 0; i<4; i++)
+	for (j = 0; j<4 - i; j++)
+	if (sum[j]<sum[j + 1])
+	{
+		temp = sum[j]; sum[j] = sum[j + 1]; sum[j + 1] = temp;
+		t = stu[j]; stu[j] = stu[j + 1]; stu[j + 1] = t;
+	}
+	for (i = 0; i < 5; i++)
+	{
+		printf("%d	%s	%4d%4d%4d\n", stu[i].num, stu[i].name, stu[i].chinese, stu[i].math, stu[i].english);
+	}
 }
 int main(void)
 {
 	int i;
-	for(i=0;i<5;i++)
+	for (i = 0; i<5; i++)
 	{
 		printf("学号:");
-		scanf("%d",&stu[i].num);
+		scanf("%d", &stu[i].num);
 		printf("姓名:");
-		scanf("%s",&stu[i].name);
+		scanf("%s", &stu[i].name);
 		printf("语文成绩:");
-		scanf("%d",&stu[i].chinese);
+		scanf("%d", &stu[i].chinese);
 		printf("数学成绩:");
-		scanf("%d",&stu[i].math);
+		scanf("%d", &stu[i].math);
 		printf("英语成绩:");
-		scanf("%d",&stu[i].english);
+		scanf("%d", &stu[i].english);
 	}
 	print();
 	return 0;

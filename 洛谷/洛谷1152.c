ed6765a81573da69a37012a9eff93cffa663f8e1
kaminna <71122975+kaminna@users.noreply.github.com>
2021-03-07 08:30:12 +0800

    Update 洛谷1152.c

--- 洛谷/洛谷1152.c
+++ 洛谷/洛谷1152.c
@@ -2,23 +2,21 @@
 #include <math.h>
 int main(int argc, char** argv)
 {
-	int n, i, diff, next, flag = 1;
+	int n, i, diff, flag = 1;
 	scanf("%d", &n);
 	int nums[1000];
 	for (i = 0; i < n; i++)
 	{
 		scanf("%d", &nums[i]);
 	}
-	diff = fabs(nums[0] - nums[1]);
-	for (i = 1; i+1 < n; i++)
+	for (i = 0; i+1 < n; i++)
 	{
-		next = fabs(nums[i] - nums[i + 1]);
-		if (next != diff - 1)
+		diff = fabs(nums[i] - nums[i + 1]);
+		if (diff > n-1 || diff <1)
 		{
 			flag = 0;
 			break;
 		}
-		diff--;
 	}
 	if (flag)
 	{

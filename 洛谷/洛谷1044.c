4712a5b4c166b3a2fd2d75da5aba7a583b26d61e
kaminna <71122975+kaminna@users.noreply.github.com>
2021-03-12 22:04:18 +0800

    Create 洛谷1044.c

---
+++ 洛谷/洛谷1044.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main(int argc, char** argv)
+{
+	int n, h[20] = { 1,1 };
+	scanf("%d", &n);
+	for (int i = 2; i <= n; i++)
+	{
+		for (int j = 0; j < i; j++)
+		{
+			h[i] += h[j] * h[i - j - 1];
+		}
+	}
+	printf("%d", h[n]);
+	return 0;
+}

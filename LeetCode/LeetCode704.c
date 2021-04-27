9e8da8190ed690bbb775308d208d9297557f7bdc
kaminna <71122975+kaminna@users.noreply.github.com>
2021-04-27 15:07:51 +0800

    Create LeetCode704.c

---
+++ LeetCode/LeetCode704.c
@@ -0,0 +1,16 @@
+int search(int* nums, int numsSize, int target) {
+	int left = 0, right = numsSize - 1;
+	while (left <= right) {
+		int mid = left + (right - left) / 2;
+		if (nums[mid] == target) {
+			return mid;
+		}
+		if (nums[mid] < target) {
+			left = mid + 1;
+		}
+		if (nums[mid] > target) {
+			right = mid - 1;
+		}
+	}
+	return -1;
+}

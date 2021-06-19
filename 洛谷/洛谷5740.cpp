70744bbfa13e98b7c5e2ef34bd0cb2e3add19c67
kaminna <71122975+kaminna@users.noreply.github.com>
2021-06-19 19:10:09 +0800

    Create 洛谷5740.cpp

---
+++ 洛谷/洛谷5740.cpp
@@ -0,0 +1,27 @@
+#include <cstdio>
+bool isPrime(int x) {
+	if (x <= 0) {
+		return false;
+	}
+	if (x == 1) {
+		return false;
+	}
+	for (int i = 2; i < x; i++) {
+		if (x % i == 0) {
+			return false;
+		}
+	}
+	return true;
+}
+int main() {
+	int n;
+	scanf("%d", &n);
+	int nums[101];
+	for (int i = 0; i < n; i++) {
+		scanf("%d", &nums[i]);
+		if (isPrime(nums[i])) {
+			printf("%d ", nums[i]); 
+		}
+ 	}
+	return 0;
+}

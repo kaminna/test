1e9374f06caa15caca12a5963363b044a2933db6
kaminna <71122975+kaminna@users.noreply.github.com>
2021-07-15 19:19:02 +0800

    Create 洛谷B2004.cpp

---
+++ 洛谷/洛谷B2004.cpp
@@ -0,0 +1,9 @@
+#include <cstdio>
+int main(int argc, char** argv) {
+	long long a, b, c;
+	scanf("%lld%lld%lld", &a, &b, &c);
+	printf("%8lld ", a);
+	printf("%8lld ", b);
+	printf("%8lld", c);
+	return 0;
+}

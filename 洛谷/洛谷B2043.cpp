c44b5306968b83cf137c0f9900a5e31818bcfcce
kaminna <71122975+kaminna@users.noreply.github.com>
2021-07-16 15:55:12 +0800

    Create 洛谷B2043.cpp

---
+++ 洛谷/洛谷B2043.cpp
@@ -0,0 +1,30 @@
+#include <cstdio>
+int main(int argc, char** argv) {
+	int x;
+	scanf("%d", &x);
+	if (x % 105 == 0) {
+		printf("3 5 7");
+	}
+	else if (x % 15 == 0) {
+		printf("3 5");
+	}
+	else if (x % 21 == 0) {
+		printf("3 7");
+	}
+	else if (x % 35 == 0) {
+		printf("5 7");
+	}
+	else if (x % 3 == 0) {
+		printf("3");
+	}
+	else if (x % 5 == 0) {
+		printf("5");
+	}
+	else if (x % 7 == 0) {
+		printf("7");
+	}
+	else {
+		printf("n");
+	}
+	return 0;
+}

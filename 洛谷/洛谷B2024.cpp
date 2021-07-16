f22adddb227043cacac83128823a4d2696cb68cb
kaminna <71122975+kaminna@users.noreply.github.com>
2021-07-16 15:54:35 +0800

    Create 洛谷B2024.cpp

---
+++ 洛谷/洛谷B2024.cpp
@@ -0,0 +1,10 @@
+#include <cstdio>
+int main(int argc, char** argv) {
+	double var;
+	scanf("%lf", &var);
+	printf("%f\n", var);
+	printf("%.5f\n", var);
+	printf("%e\n", var);
+	printf("%g", var);
+	return 0;
+}

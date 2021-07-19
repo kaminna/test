f72b2c31cb7b1538e3a90e5330a6d8b3991869fb
kaminna <71122975+kaminna@users.noreply.github.com>
2021-07-19 15:33:14 +0800

    Create 洛谷B2082.cpp

---
+++ 洛谷/洛谷B2082.cpp
@@ -0,0 +1,16 @@
+#include <cstdio>
+int main(int argc, char** argv) {
+	int L, R, cnt = 0;
+	scanf("%d%d", &L, &R);
+	for (int i = L; i <= R; i++) {
+		int t = i;
+		while (t) {
+			if (t % 10 == 2) {
+				cnt++;
+			}
+			t /= 10;
+		}
+	}
+	printf("%d", cnt);
+	return 0;
+}

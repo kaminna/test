0978496f5b66ac1d520eaf57770d3167cca7999a
kaminna <71122975+kaminna@users.noreply.github.com>
2021-07-19 17:38:23 +0800

    Update 洛谷B2091.cpp

--- 洛谷/洛谷B2091.cpp
+++ 洛谷/洛谷B2091.cpp
@@ -1,5 +1,14 @@
 #include <cstdio>
 int main(int argc, char** argv) {
-
+	int n, m, nums[1000], ans = 0;
+	scanf("%d", &n);
+	for (int i = 0; i < n; i++) {
+		scanf("%d", &nums[i]);
+	}
+	for (int i = 0; i < n; i++) {
+		scanf("%d", &m);
+		ans += nums[i] * m;
+	}
+	printf("%d", ans);
 	return 0;
 }

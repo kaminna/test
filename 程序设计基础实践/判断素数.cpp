679c26310d733049921733fc668cde1e41a7f975
kaminna <71122975+kaminna@users.noreply.github.com>
2021-07-21 17:45:35 +0800

    Update 判断素数.cpp

--- 程序设计基础实践/判断素数.cpp
+++ 程序设计基础实践/判断素数.cpp
@@ -1,13 +1,9 @@
 #include <cstdio>
-#include <cmath>
 bool isPrime(long long x) {
-	if (x <= 0) {
+	if (x <= 1) {
 		return false;
 	}
-	if (x == 1) {
-		return false;
-	}
-	for (long long i = 2; i < sqrt(x); i++) {
+	for (long long i = 2; i*i <= x; i++) {
 		if (x % i == 0) {
 			return false;
 		}

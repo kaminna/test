ab280c9ad99a4d9f344037f2db288b82ba16ebd9
kaminna <71122975+kaminna@users.noreply.github.com>
2021-07-22 14:43:24 +0800

    Create 洛谷B2147.cpp

---
+++ 洛谷/洛谷B2147.cpp
@@ -0,0 +1,15 @@
+#include <cstdio>
+#include <cmath>
+double f(double x, int n) {
+	if (n == 1) {
+		return sqrt(1 + x);
+	}
+	return sqrt(n + f(x, n - 1));
+}
+int main(int argc, char** argv) {
+	double x;
+	int n;
+	scanf("%lf%d", &x, &n);
+	printf("%.2lf", f(x, n));
+	return 0;
+}

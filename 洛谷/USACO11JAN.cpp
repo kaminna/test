64640c96f16cd3d87881f1e096e075df394bd7ab
kaminna <71122975+kaminna@users.noreply.github.com>
2021-08-08 23:36:53 +0800

    Create USACO11JAN.cpp

---
+++ 洛谷/USACO11JAN.cpp
@@ -0,0 +1,16 @@
+#include <cstdio>
+#include <algorithm>
+using namespace std;
+int n, a[100001], ans = -0x7fffffff;
+int main()
+{
+	scanf("%d", &n);
+	for (register int i = 1; i <= n; i++)
+	{
+		scanf("%d", &a[i]);
+		a[i] = max(a[i], a[i] + a[i - 1]);
+		ans = max(a[i], ans);
+	}
+	printf("%d\n", ans);
+	return 0;
+}

65a9a0dc4513bb8b5cfe9e4eff8d67b52df12888
kaminna <71122975+kaminna@users.noreply.github.com>
2021-08-14 17:16:49 +0800

    Update LeetCode53.c

--- LeetCode/LeetCode53.c
+++ LeetCode/LeetCode53.c
@@ -15,3 +15,13 @@ int maxSubArray(int* nums, int numsSize)
 	}
 	return max;
 }
+/* dp
+int maxSubArray(int* nums, int numsSize) {
+	int pre = 0, maxAns = nums[0];
+	for (int i = 0; i < numsSize; i++) {
+		pre = fmax(pre + nums[i], nums[i]);
+		maxAns = fmax(maxAns, pre);
+	}
+	return maxAns;
+}
+*/

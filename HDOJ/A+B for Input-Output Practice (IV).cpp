dde0436f5305f83f7ae2c98e4f58160f9b275349
kaminna <71122975+kaminna@users.noreply.github.com>
2021-08-21 00:13:15 +0800

    Create A+B for Input-Output Practice (IV).cpp

---
+++ HDOJ/A+B for Input-Output Practice (IV).cpp
@@ -0,0 +1,14 @@
+#include <cstdio>
+int main(int argc, char** argv) {
+	int n;
+	while (scanf("%d", &n) == 1) {
+		if (n == 0) break;
+		long long nums, ans = 0;
+		for (int i = 0; i < n; i++) {
+			scanf("%lld", &nums);
+			ans += nums;
+		}
+		printf("%lld\n", ans);
+	}
+	return 0;
+}

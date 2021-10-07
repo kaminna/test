b6024a6f4946bfad7e2b4fb73261862040112667
kaminna <71122975+kaminna@users.noreply.github.com>
2021-10-07 18:21:39 +0800

    Update D.cpp

--- AtCoder/ABC/042/D.cpp
+++ AtCoder/ABC/042/D.cpp
@@ -1,19 +1,32 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#define Mod 1000000007
 using namespace std;
-long long ans;
-void f(int x, int y, int H, int W, int A, int B) {
-	if (x == H && y == W) {
-		ans++;
+long long fac[100001];
+long long fastPower(long long base, long long power) {
+	long long result = 1;
+	while (power > 0) {
+		if (power & 1) {	//等价于if(power%2==1)
+			result = result * base % Mod;
+		}
+		power >>= 1;	//等价于power/=2
+		base = (base * base) % Mod;
 	}
-	if (x != W && x != B)
-	f(x++, y, H, W, A, B);
-	if (y != H && y != A)
-	f(x, y++, H, W, A, B);
+	return result;
+}
+long long Pow(long long n) {
+	if (fac[n]) {
+		return fac[n];
+	}
+	return fac[n] = (Pow(n - 1) % Mod) * (n%Mod) % Mod;
 }
 int main() {
-	int H, W, A, B;
-	cin >> H >> W >> A >> B; 
-	f(0, 0, H, W, A, B);
-	cout << ans << endl;
+	ios::sync_with_stdio(false);
+	//int H, W, A, B;
+	//cin >> H >> W >> A >> B;
+	fac[0] = 1;
+	fac[1] = 1;
+	fac[2] = 2;
+	fac[3] = 6;
+	fac[4] = 24;
 	return 0;
 }

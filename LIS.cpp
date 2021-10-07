c1d52e748f08f495e910afd2d5f18f520e4891cb
kaminna <71122975+kaminna@users.noreply.github.com>
2021-10-07 18:15:44 +0800

    Update LIS.cpp

--- LIS.cpp
+++ LIS.cpp
@@ -1,24 +1,32 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<algorithm>
+#include <cstdio>
+#include <algorithm>
+#include <vector>
 using namespace std;
-int a[100];
-int dp[100];
-int main(void)
-{
-	int n;
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        vector<int> res;
+        for(auto n : nums) {
+            auto lb = lower_bound(res.begin(), res.end(), n);
+            if(lb == res.end()) {
+                res.push_back(n);
+            }
+            else {
+                res[lb - res.begin()] = n;
+            }
+        }
+        return res.size();
+    }
+};
+int main(int argc, char** argv) {
+	int n, t;
+    Solution sl;
+    vector<int> vc; 
 	scanf("%d", &n);
-	for (int i = 1; i <= n; i++)
-		scanf("%d", &a[i]);
-	for (int i = 1; i <= n; i++)
-	{
-		dp[i] = 1;
-		for (int j = 1; j < i; j++)
-		{
-			if (a[j] < a[i])
-				dp[i] = max(dp[i], dp[j] + 1);
-		}
-		printf("dp[%d]=%d\n", i, dp[i]);
-	}
+	for (int i = 1; i <= n; i++) {
+		scanf("%d", &t);
+        vc.push_back(t);
+    }
+	printf("%d\n", sl.lengthOfLIS(vc));
 	return 0;
 }

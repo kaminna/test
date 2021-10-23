1f161914d2e489abe3e48c4d4ae08023d6effc57
kaminna <71122975+kaminna@users.noreply.github.com>
2021-10-23 12:28:28 +0800

    Create 洛谷1540.cpp

---
+++ 洛谷/洛谷1540.cpp
@@ -0,0 +1,94 @@
+#include <cstdio>
+#include <cstdlib>
+#define OK 1
+#define ERROR 0
+typedef int Status;
+typedef int QElemType;
+typedef struct QNode {
+    QElemType data;
+    struct QNode *next;
+}QNode, *QueuePtr;
+typedef struct {
+    QueuePtr front;
+    QueuePtr rear;
+}LinkQueue;
+Status InitQueue(LinkQueue &Q) {
+    Q.front = Q.rear = (QueuePtr)malloc(sizeof(QNode));
+    if (!Q.front) return ERROR;
+    Q.front->next = NULL;
+    return OK;
+}
+Status DestroyQueue(LinkQueue &Q) {
+    while (Q.front) {
+        Q.rear = Q.front->next;
+        free(Q.front);
+        Q.front = Q.rear;
+    }
+    return OK;
+}
+
+int QueueLength(LinkQueue Q) {
+    int cnt = 0;
+    QueuePtr p = Q.front->next;
+    while (p) {
+        p = p->next;
+        cnt++;
+    }
+    return cnt;
+}
+
+Status EnQueue(LinkQueue &Q, QElemType e) {
+    QueuePtr p = (QueuePtr)malloc(sizeof(QNode));
+    if (!p) return ERROR;
+    p->data = e;
+    p->next = NULL;
+    Q.rear->next = p;
+    Q.rear = p;
+    return OK;
+}
+Status DeQueue(LinkQueue &Q, QElemType &e) {
+    if (Q.front == Q.rear) return ERROR;
+    QueuePtr p = Q.front->next;
+    e = p->data;
+    Q.front->next = p->next;
+    if (Q.rear == p) Q.rear = Q.front;
+    free(p);
+    return OK;
+}
+
+int LocateElem(LinkQueue Q, QElemType e) {
+    QueuePtr p = Q.front->next;
+    int cnt = 1;
+    while (p) {
+        if (p->data == e) {
+            return cnt;
+        }
+        p = p->next;
+        cnt++;
+    }
+    return 0;
+}
+int main(int argc, char** argv) {
+    int n, m, cnt = 0;
+    QElemType a, e;
+    LinkQueue Q;
+    InitQueue(Q);
+    scanf("%d%d", &m, &n);
+    for (int i = 1; i <= n; i++) {
+        scanf("%d", &a);
+        if (QueueLength(Q) < m && !LocateElem(Q, a)) {
+            cnt++;
+            EnQueue(Q, a);
+        }
+        else {
+            if (!LocateElem(Q, a)) {
+                cnt++;
+                DeQueue(Q, e);
+                EnQueue(Q, a);
+            }
+        }
+    }
+    printf("%d", cnt);
+    DestroyQueue(Q);
+    return 0;
+}

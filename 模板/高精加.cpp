4a90468296e8c6b0b6b4ec120f9468ce4cc3c250
kaminna <71122975+kaminna@users.noreply.github.com>
2021-10-31 18:08:52 +0800

    Update 高精加.cpp

--- 模板/高精加.cpp
+++ 模板/高精加.cpp
@@ -1,58 +1,21 @@
-#include <cstdio>
-#include <cstring>
+#include <bits/stdc++.h>
+using namespace std;
+string add(string a,string b) {
+    const int L=1e5;
+    string ans;
+    int na[L]={0},nb[L]={0};
+    int la=a.size(),lb=b.size();
+    for(int i=0;i<la;i++) na[la-1-i]=a[i]-'0';
+    for(int i=0;i<lb;i++) nb[lb-1-i]=b[i]-'0';
+    int lmax=la>lb?la:lb;
+    for(int i=0;i<lmax;i++) na[i]+=nb[i],na[i+1]+=na[i]/10,na[i]%=10;
+    if(na[lmax]) lmax++;
+    for(int i=lmax-1;i>=0;i--) ans+=na[i]+'0';
+    return ans;
+}
 int main(int argc, char** argv) {
-	char a[1000], b[1000], sum[1000];
-	memset(sum, '0', sizeof(sum));
-	scanf("%s%s", a, b);
-	int len1 = strlen(a), len2 = strlen(b), i = 0;
-	len1--;
-	len2--;
-	while (len1 != -1 && len2 != -1) {
-		if (sum[i] > '9') {
-			sum[i] -= 10;
-			sum[i + 1]++;
-		}
-		sum[i] += a[len1] + b[len2] - 2 * '0';
-		if (sum[i] > '9') {
-			sum[i] -= 10;
-			sum[i + 1]++;
-		}
-		len1--;
-		len2--;
-		i++;
-	}
-	while (len1 != -1) {
-		if (sum[i] > '9') {
-			sum[i] -= 10;
-			sum[i + 1]++;
-		}
-		sum[i] += a[len1] - '0';
-		if (sum[i] > '9') {
-			sum[i] -= 10;
-			sum[i + 1]++;
-		}
-		len1--;
-		i++;
-	}
-	while (len2 != -1) {
-		if (sum[i] > '9') {
-			sum[i] -= 10;
-			sum[i + 1]++;
-		}
-		sum[i] += b[len2] - '0';
-		if (sum[i] > '9') {
-			sum[i] -= 10;
-			sum[i + 1]++;
-		}
-		len2--;
-		i++;
-	}
-	if (sum[i] == '0') {
-		i--;
-	}
-	while (i != -1) {
-		printf("%c", sum[i]);
-		i--;
-	}
+	string a, b;
+	cin >> a >> b;
+	cout << add(a, b) << endl;
 	return 0;
 }

b12eb7c69e0b58a83bc05c9b1a1bf802dcd2708b
kaminna <71122975+kaminna@users.noreply.github.com>
2021-11-16 21:01:13 +0800

    Update 判断素数.cpp

--- 模板/判断素数.cpp
+++ 模板/判断素数.cpp
@@ -1,24 +1,6 @@
-#include <cstdio>
 bool isPrime(long long x) {
-	if (x <= 1) {
-		return false;
-	}
-	for (long long i = 2; i*i <= x; i++) {
-		if (x % i == 0) {
-			return false;
-		}
-	}
+	if (x <= 1) return false;
+	for (long long i = 2; i*i <= x; i++)
+		if (!(x % i)) return false;
 	return true;
 }
-
-int main() {
-	long long n;
-	scanf("%lld", &n);
-	for (long long i = 2; i <= n; i++) {
-		if (isPrime(i)) {
-			printf("%lld ", i);
-		}
-	}
-	return 0;
-}
-

0a54fae5614d43c6609c17106e14c90009945819
kaminna <71122975+kaminna@users.noreply.github.com>
2021-11-16 21:01:15 +0800

    Update 快速幂.cpp

--- 模板/快速幂.cpp
+++ 模板/快速幂.cpp
@@ -2,11 +2,9 @@
 long long fastPower(long long base, long long power) {
     long long result = 1;
     while (power > 0) {
-        if (power & 1) {//此处等价于if(power%2==1)
-            result = result * base % MOD;
-        }
-        power >>= 1;//此处等价于power=power/2
-        base = (base * base) % MOD;
+        if (power & 1) result = result * base % MOD;
+        power >>= 1;
+        base = base * base % MOD;
     }
     return result;
 }

24c03e0127ba6332bffdba64a4fe65c2e60c3abb
kaminna <71122975+kaminna@users.noreply.github.com>
2021-12-08 18:00:31 +0800

    Update 洛谷1208.cpp

--- 洛谷/洛谷1208.cpp
+++ 洛谷/洛谷1208.cpp
@@ -1,22 +1,33 @@
-#include <cstdio>
-int ans[100000];
-int f(int n) {
-	if (ans[n]) {
-		return ans[n];
-	}
-	if (n % 2) {
-		return ans[n] = f(n-1);
-	}
-	else {
-		return ans[n] = f(n-1)+f(n/2);
-	}
+#include <bits/stdc++.h>
+using namespace std;
+typedef struct {
+    int p;
+    int a;
+}info;
+bool cmp(info x, info y) {
+    if (x.p == y.p) return x.a > y.a;
+    return x.p < y.p;
 }
-int main() {
-	int n;
-	scanf("%d", &n);
-	ans[0] = 1;
-	ans[1] = 1;
-	printf("%d", f(n));
-	return 0;
+int main(int argc, char** argv) {
+    int n, m, t;
+    long long ans = 0;
+    info nums[10000];
+    scanf("%d%d", &n, &m);
+    t = n;
+    for (int i = 0; i < m; i++) {
+        scanf("%d%d", &nums[i].p, &nums[i].a);
+    }
+    sort(nums, nums + m, cmp);
+    for (int i = 0; i < n; i++) {
+        if (t > nums[i].a) {
+            ans += nums[i].a * nums[i].p;
+            t -= nums[i].a;
+        }
+        else {
+            ans += nums[i].p * t;
+            break;
+        }
+    }
+    printf("%lld", ans);
+    return 0;
 }
-

0b572f5ee8de7c18ac5091bebb3f5db0d2390b66
kaminna <71122975+kaminna@users.noreply.github.com>
2022-02-24 23:13:57 +0800

    Create LeetCode1706.cpp

---
+++ LeetCode/LeetCode1706.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> findBall(vector<vector<int>> &grid) {
+        int n = grid[0].size();
+        vector<int> ans(n);
+        for (int j = 0; j < n; ++j) {
+            int col = j; // 球的初始列
+            for (auto &row : grid) {
+                int dir = row[col];
+                col += dir; // 移动球
+                if (col < 0 || col == n || row[col] != dir) { // 到达侧边或 V 形
+                    col = -1;
+                    break;
+                }
+            }
+            ans[j] = col; // col >= 0 为成功到达底部
+        }
+        return ans;
+    }
+};
